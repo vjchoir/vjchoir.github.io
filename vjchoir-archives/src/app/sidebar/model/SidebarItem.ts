65a365cadbf1a37c32815e706da50c9796518a4d
jumpcutfindo <danielhoe97@gmail.com>
2020-05-12 15:18:12 +0800

    Added sidebar service and item

---
+++ vjchoir-archives/src/app/sidebar/model/SidebarItem.ts
@@ -0,0 +1,4 @@
+export interface SidebarItem {
+    id: number;
+    title: string;
+}
--- vjchoir-archives/src/app/sidebar/sidebar.component.ts
+++ vjchoir-archives/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarItem } from './model/SidebarItem';
 
 @Component({
   selector: 'app-sidebar',
