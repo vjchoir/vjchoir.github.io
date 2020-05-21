cf077b4b2848fdf55fefd3bd4d2e8027794d1713
jumpcutfindo <danielhoe97@gmail.com>
2020-05-21 12:05:59 +0800

    Fixed to be compatible with SCSS

--- vjchoir-archives/src/app/sidebar/sidebar.component.ts
+++ vjchoir-archives/src/app/sidebar/sidebar.component.ts
@@ -4,7 +4,7 @@ import { SidebarItem } from './model/SidebarItem';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
 
