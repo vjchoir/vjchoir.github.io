bda0e26230359ea322fa6abd30d3ac40de6265c3
jumpcutfindo <danielhoe97@gmail.com>
2020-05-24 01:24:05 +0800

    Added padding and sidebar scrolls

--- vjchoir-archives/src/app/pages/home/home.component.ts
+++ vjchoir-archives/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import homeJSON from '../../../assets/data/home.json';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private homeJSON;
+
   constructor() { }
 
   ngOnInit() {
+    this.homeJSON = homeJSON;
   }
 
 }
