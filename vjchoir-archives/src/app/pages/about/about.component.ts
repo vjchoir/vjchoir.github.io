4ad972b5fc143282813d72c622135c8a2a1e251d
jumpcutfindo <danielhoe97@gmail.com>
2020-05-24 05:10:51 +0800

    Updated about component

--- vjchoir-archives/src/app/pages/about/about.component.ts
+++ vjchoir-archives/src/app/pages/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import aboutJSON from '../../../assets/data/about.json';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  defaultActiveIndex = 0;
+
+  aboutJSON: any;
+  currActive;
+
   constructor() { }
 
   ngOnInit() {
+    this.aboutJSON = aboutJSON;
+    
+    this.currActive = this.aboutJSON[this.defaultActiveIndex];
   }
 
 }
