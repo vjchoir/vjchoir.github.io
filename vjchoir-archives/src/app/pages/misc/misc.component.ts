8ef900d5aace1ea50cb24a131447377f4bde5732
jumpcutfindo <danielhoe97@gmail.com>
2020-06-10 14:03:36 +0800

    Added misc page

--- vjchoir-archives/src/app/pages/misc/misc.component.ts
+++ vjchoir-archives/src/app/pages/misc/misc.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MiscService } from './misc.service';
+import { UpdateLog } from './model/UpdateLog';
 
 @Component({
   selector: 'app-misc',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiscComponent implements OnInit {
 
-  constructor() { }
+  updateLog: UpdateLog[];
+  miscJSON;
+
+  currActive;
+
+  constructor(private miscService: MiscService) { }
 
   ngOnInit() {
+    this.miscService.getUpdateLog().subscribe(updateLog => this.updateLog = updateLog);
+    this.miscService.getContent().subscribe(miscJSON => this.miscJSON = miscJSON);
+
+    this.currActive = this.miscJSON.sections[0];
   }
 
 }
