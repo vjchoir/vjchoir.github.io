43227a1978b6ba0f1b598e845e288f81bc1febc0
jumpcutfindo <danielhoe97@gmail.com>
2020-06-10 14:15:13 +0800

    Made some minor adjustments to misc

--- vjchoir-archives/src/app/pages/misc/misc.component.ts
+++ vjchoir-archives/src/app/pages/misc/misc.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MiscService } from './misc.service';
 import { UpdateLog } from './model/UpdateLog';
+import { Router } from '@angular/router';
+import { NavControllerService } from 'src/app/navigation/nav-controller/nav-controller.service';
 
 @Component({
   selector: 'app-misc',
@@ -14,13 +16,33 @@ export class MiscComponent implements OnInit {
 
   currActive;
 
-  constructor(private miscService: MiscService) { }
+  constructor(private miscService: MiscService, private navControllerService: NavControllerService, private router: Router) { }
 
   ngOnInit() {
     this.miscService.getUpdateLog().subscribe(updateLog => this.updateLog = updateLog);
-    this.miscService.getContent().subscribe(miscJSON => this.miscJSON = miscJSON);
+    this.miscService.getContent().subscribe(miscJSON => {
+      this.miscJSON = miscJSON
+      this.handleFragment();
+    });
 
-    this.currActive = this.miscJSON.sections[0];
+    this.navControllerService.routerUpdates.subscribe(val => {
+      this.handleFragment();
+    });
+
+    
   }
 
+  private handleFragment() {
+    if (!this.router.url.includes("#")) {
+      this.currActive = this.miscJSON.sections[0];
+    } else {
+      let fragment = this.router.url.split("#")[1];
+      for (let item of this.miscJSON.sections) {
+        if (fragment.includes(item.id)) {
+          this.currActive = item;
+          return;
+        }
+      }
+    }
+  }
 }
